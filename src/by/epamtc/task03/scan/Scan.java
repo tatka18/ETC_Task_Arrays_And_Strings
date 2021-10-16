b0f26086197b8a3b373fec00e6aa7b7906b54bc4
tatka18 <tatka19871218@gmail.com>
2021-10-16 11:48:05 +0300

    Class Scan with readListFromTextFile() method for creating integer list from file has been added

---
+++ src/by/epamtc/task03/scan/Scan.java
@@ -0,0 +1,23 @@
+package by.epamtc.task03.scan;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Scan {
+    public static List<Integer> readListFromTextFile() throws IOException {
+        String fileName = "C:\\Folder for HD\\Studying\\Code Projects\\ETC_Task3_Arrays_And_Strings\\src\\by\\epamtc\\task03\\util\\numbers";
+        Path path = Paths.get(fileName);
+        Scanner scanner = new Scanner(path);
+
+        List<Integer> integerList = new ArrayList<>();
+
+        while ((scanner.hasNext())){
+            integerList.add(scanner.nextInt());
+        }
+        return integerList;
+    }
+}
