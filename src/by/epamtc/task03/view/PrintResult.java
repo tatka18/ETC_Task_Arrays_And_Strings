098f3ff4f6679889feb911431a92a11b8b3a408f
tatka18 <tatka19871218@gmail.com>
2021-10-16 11:50:32 +0300

    method printListFromFile() has been added

---
+++ src/by/epamtc/task03/view/PrintResult.java
@@ -0,0 +1,9 @@
+package by.epamtc.task03.view;
+
+import java.util.List;
+
+public class PrintResult {
+    public static void printListFromFile(List<Integer> list){
+        System.out.println(list.toString());
+    }
+}
