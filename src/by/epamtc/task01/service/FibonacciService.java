6491203b0599cd1d2453d5b14e244ed976bb2396
tatka18 <tatka19871218@gmail.com>
2021-10-17 23:40:40 +0300

    fibonacciNumbersFromArray() method has been added

--- src/by/epamtc/task01/service/FibonacciService.java
+++ src/by/epamtc/task01/service/FibonacciService.java
@@ -1,5 +1,22 @@
 package by.epamtc.task01.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FibonacciService {
+    public static List<Integer> fibonacciNumbersFromArray(int [] array){
+        List<Integer> integerList = new ArrayList<>();
+
+        int [] fibonacciNumbers = fibonacciNumbers();
+        for(int number : fibonacciNumbers){
+            boolean result = IntegerArrayService.binarySearch(array, number);
+            if(result){
+                integerList.add(number);
+            }
+        }
+        return integerList;
+    }
+
     public static int[] fibonacciNumbers(){
         int[] arr = new int[47];
         arr[0] = 0;
