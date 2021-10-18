be30fc9e841bcc24c1d62647724386de4eb47e95
tatka18 <tatka19871218@gmail.com>
2021-10-19 01:15:16 +0300

    printResult() method has been added

---
+++ src/by/epamtc/task02/view/PrintResult.java
@@ -0,0 +1,9 @@
+package by.epamtc.task02.view;
+
+import java.util.Arrays;
+
+public class PrintResult {
+    public static void printResult(int [][] array){
+        System.out.println(Arrays.deepToString(array));
+    }
+}
