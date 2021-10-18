132b22f2443c2d5238e4f1fa8bb045bdf7db432f
tatka18 <tatka19871218@gmail.com>
2021-10-19 01:28:05 +0300

    method main() for task03 was edited and supplemented with elements to work with methods of tasks

--- src/by/epamtc/task03/run/Main.java
+++ src/by/epamtc/task03/run/Main.java
@@ -1,15 +1,32 @@
 package by.epamtc.task03.run;
 
-import by.epamtc.task03.scan.Scan;
+import by.epamtc.task03.service.StringService;
+import by.epamtc.task03.service.StringServiceWithRegex;
 import by.epamtc.task03.view.PrintResult;
 
-import java.io.*;
-import java.util.List;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        List<Integer> integerList = Scan.readListFromTextFile();
-        PrintResult.printListFromFile(integerList);
 
+        PrintResult.printChangedText(StringService.changeLetter( 5, 'D'));
+
+        PrintResult.printChangedText(StringService.changeWord(9, "Zagagulina"));
+
+        PrintResult.printChangedText(StringService.deleteWordsStartsWithConsonant(7));
+
+        PrintResult.printChangedText(StringService.changeWrongLetter("ро", "А"));
+
+        ////////////////////////WITH REGEX
+
+        PrintResult.printChangedText(StringServiceWithRegex.changeLetter(6, 'Й'));
+
+        PrintResult.printChangedText(StringServiceWithRegex.deleteWhatIsNotLetterAndSpace());
+
+        PrintResult.printChangedText(StringServiceWithRegex.changeWrongLetter("String", "A"));
+
+        PrintResult.printChangedText(StringServiceWithRegex.changeWord(5, "Gaagovich"));
+
+        PrintResult.printChangedText(StringServiceWithRegex.deleteWordsStartsWithConsonant(5));
     }
 }
